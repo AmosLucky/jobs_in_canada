df737e6052cea72fef6eb768662fa13d424c8318
Amos Lucky <aluckya100@gmail.com>
2023-07-09 23:45:44 +0100

    Fixed same anonymouse user issue

--- lib/repos/user_repo.dart
+++ lib/repos/user_repo.dart
@@ -128,9 +128,23 @@ class UserRepo extends ChangeNotifier {
       signInWithId(user.uid, context);
     } else {
       //NavigateLeftToRight(context, WelcomeScreen());
-      FirebaseAuth.instance.signInAnonymously();
+      try {
+        UserCredential? userCredential =
+            await FirebaseAuth.instance.signInAnonymously();
 
-      signInWithId(constantUserId, context);
+        FirebaseFirestore.instance
+            .collection(user_database)
+            .doc(userCredential.user!.uid)
+            .set({
+          "username": "anonymous",
+          "user_id": userCredential.user!.uid,
+          "email": "anonymous0@gmail.com"
+        });
+
+        signInWithId(userCredential.user!.uid, context);
+      } catch (e) {
+        signInWithId(constantUserId, context);
+      }
     }
   }
 
@@ -221,34 +235,33 @@ class UserRepo extends ChangeNotifier {
       case "account-exists-with-different-credential":
       case "email-already-in-use":
         return "Email already used. Go to login page.";
-       
+
       case "ERROR_WRONG_PASSWORD":
       case "wrong-password":
         return "Wrong email/password combination.";
-        
+
       case "ERROR_USER_NOT_FOUND":
       case "user-not-found":
         return "No user found with this email.";
-        
+
       case "ERROR_USER_DISABLED":
       case "user-disabled":
         return "User disabled.";
-        
+
       case "ERROR_TOO_MANY_REQUESTS":
       case "operation-not-allowed":
         return "Too many requests to log into this account.";
-        
+
       case "ERROR_OPERATION_NOT_ALLOWED":
       case "operation-not-allowed":
         return "Server error, please try again later.";
-        
+
       case "ERROR_INVALID_EMAIL":
       case "invalid-email":
         return "Email address is invalid.";
-       
+
       default:
         return "Login failed. Check email and password.";
-        
     }
   }
 }
